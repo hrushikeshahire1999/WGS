4f21a46a4b7e163d34bc29b3000edcafcc0c6b34
hrushikeshahire1999 <hrushikeshahire1999@gmail.com>
2022-08-18 16:16:44 +0530

    added files

---
+++ src/Demo/Calculator.java
@@ -0,0 +1,14 @@
+package Demo;
+
+
+
+public class Calculator {
+    public static void main(String args[])
+    {
+        System.out.println("Addition");
+        Math.add(22,67);
+        Math.add(65,89,43);
+        System.out.println("Multiplication");
+        Math.multiply(23,8);
+    }
+}
